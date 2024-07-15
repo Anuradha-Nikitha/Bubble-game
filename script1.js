4928014b108ffa5d500380a83d80698aa02f5e0a
Anuradha-Nikitha <132152567+Anuradha-Nikitha@users.noreply.github.com>
2024-07-15 15:43:33 +0530

    script1.js

--- script1.js
+++ script1.js
@@ -4,12 +4,12 @@ var hitrn = 0;
 
 function increaseScore(){
     score +=10;
-    document.querySelector("#scoreval").textContent = score;
+    document.querySelector("#scorevalue").textContent = score;
 }
 
 function getNewHit(){
     hitrn = Math.floor(Math.random()*10);
-    document.querySelector("#hitval").textContent = hitrn;
+    document.querySelector("#hitvalue").textContent = hitrn;
 }
 
 function makeBubble(){
@@ -20,23 +20,23 @@ for(var i=1; i<=119; i++){
     clutter += `<div class="bubble">${rn}</div>`;
 }
 
-document.querySelector("#pbtm").innerHTML = clutter; 
+document.querySelector("#panel-bottom").innerHTML = clutter; 
 }
 
 function runTimer(){
     var timerint = setInterval(function () {
         if(timer > 0){
             timer--;
-            document.querySelector("#timerval").textContent = timer;
+            document.querySelector("#timervalue").textContent = timer;
         }
         else{
             clearInterval(timerint); 
-            document.querySelector("#pbtm").innerHTML = `<h1>GAME OVER</h1>`;
+            document.querySelector("#panel-bottom").innerHTML = `<h1>GAME OVER</h1>`;
         }
     }, 1000)
 }
 
-document.querySelector("#pbtm").addEventListener("click", function(dets){
+document.querySelector("#panel-bottom").addEventListener("click", function(dets){
     var clickednum = Number(dets.target.textContent);
     if(clickednum === hitrn){
         increaseScore();
